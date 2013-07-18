f527d51fc3678711aa959e5a4ad0fa39242d562d
christianseel <cs@chsmedien.de>
2013-07-18 13:22:30 +0200

    added ability to add resource fields to json output and added new check for ajax request (without GET parameter)

--- core/components/ajaxswitch/elements/plugins/AjaxSwitch.php
+++ core/components/ajaxswitch/elements/plugins/AjaxSwitch.php
@@ -6,95 +6,107 @@
  * A MODX plugin that processes ajax and non-ajax requests
  *
  * @ author chsmedien, Christian Seel
- * @ version 0.2.1 - Feb 27, 2013
+ * @ version 0.2.3 - Jul 18, 2013
  * 
  * SYSTEM EVENTS:
  *	 OnWebPagePrerender
  *
 **/
 
-$parseJsonFields = $modx->getOption('ajaxmanager.parse_json_fields', $scriptProperties, true);
+$processJsonFields = $modx->getOption('ajaxswitch.process_json_fields', $scriptProperties, true);
+$addResourceFields = $modx->getOption('ajaxswitch.add_resource_fields', $scriptProperties, 'pagetitle,longtitle,uri');
 $maxIterations= (integer) $modx->getOption('parser_max_iterations', null, 10);
 $eventName = $modx->event->name;
 
 switch($eventName) {
 
 	case 'OnWebPagePrerender':
-		switch($_GET['ajax']) {
 		
-			// ajax request
-			case 'true':
-				$resourceCache = $modx->cacheManager->getCacheProvider($modx->getOption('cache_resource_key', null, 'resource'));
-				$cacheKey = $modx->resource->getCacheKey() . '/ajaxmanager/' . $modx->resource->get('id') . '.ajax';
+		// check for ajax request (most ajax frameworks like jQuery and mooTools add a specific header)
+		if (strcasecmp('XMLHttpRequest', $_SERVER['HTTP_X_REQUESTED_WITH']) === 0) {
+		// ajax request
+		
+			$resourceCache = $modx->cacheManager->getCacheProvider($modx->getOption('cache_resource_key', null, 'resource'));
+			$cacheKey = $modx->resource->getCacheKey() . '/ajaxmanager/' . $modx->resource->get('id') . '.' . md5($addResourceFields) .  '.json';
+			
+			// check if resource is cacheable
+			if ($modx->resource->get('cacheable')) {
+				// get $fields from cache
+				$fields = $resourceCache->get($cacheKey);
+			}
+			
+			if (!$fields) { // no cached version available
 				
-				// check if resource is cacheable
-				if ($modx->resource->get('cacheable')) {
-					// get $fields from cache
-					$fields = $resourceCache->get($cacheKey);
-				}
+				$fields = array();
+				$resourceArray = $modx->resource->toArray();
 				
-				if ($fields) { // cached version available
-				
-					$fields['cached'] = 'true';
-				
-				} else { // no cached version available
-				
-					//$fields = $modx->resource->toArray();
-					$fields = array();
-					$fields['sitetitle'] = $modx->resource->getTVValue('sitetitle');
-					$fields['uri'] = $modx->resource->get('uri');
-					
-					// parse fields
-					if ($parseJsonFields) {
-						foreach ($fields as $key => $value) {
-							// Parse all cached tags
-							$modx->parser->processElementTags('', $fields[$key], false, false, '[[', ']]', array(), $maxIterations);
-						}
-					}
-									
-					// put $fields in the modx cache
-					$resourceCache->set($cacheKey,$fields,0);
-					
-					// add "uncached" value, since current output is uncached
-					$fields['cached'] = 'false';
-				}
-			   
-				// Parse uncached tags in all fields and content
-				if ($parseJsonFields) {
-					foreach ($fields as $key => $value) {
-						$modx->parser->processElementTags('', $fields[$key], true, true, '[[', ']]', array(), $maxIterations);
-					}
-				}
-				
-				$fields['content'] = $modx->resource->_output;
-						
-				$mtime= microtime();
-				$mtime= explode(" ", $mtime);
-				$mtime= $mtime[1] + $mtime[0];
-				$tsum= round(($mtime - $modx->startTime) * 1000, 0) . " ms";
-				
-				$fields['content-render-time'] = $tsum;
+				$addResourceFields = explode(',',$addResourceFields);
 
-				$modx->resource->_output = $modx->toJSON($fields);
-			break;
-			
-			
-			
-			// non-ajax request
-			default:
+				foreach($addResourceFields as $key => $value) {
+					if (isset($resourceArray[$value])) {
+						$fields[$value] = $resourceArray[$value];
+					} else {
+						// seems like we're looking for a TV
+						$fields[$value] = $modx->resource->getTVValue($value);
+					}
+				}
 				
-				// get resource output
-				$output = &$modx->resource->_output;
+				// delete content field - we're adding it later manually
+				unset($fields['content']);
 				
-				// get template wrapper name from TV
-				$templateWrapper = $modx->resource->getTVValue('templateWrapper');
-				if ($templateWrapper == "") return;
+				// parse fields
+				if ($processJsonFields) {
+					foreach ($fields as $key => $value) {
+						// Parse all cached tags
+						$modx->parser->processElementTags('', $fields[$key], false, false, '[[', ']]', array(), $maxIterations);
+					}
+				}
+								
+				// put $fields in the modx cache
+				$resourceCache->set($cacheKey,$fields,0);
 				
-				$output = $modx->parseChunk($templateWrapper,array('resource.content' => $output));
- 
-				// parse all non-cacheable and remove unprocessed tags
-				$modx->getParser()->processElementTags('', $output, true, true, '[[', ']]', array(), $maxIterations);
+				// add "uncached" value, since current output is uncached
+				$fields['cached'] = 'false';
+				
+			} else { // cached version available
+				$fields['cached'] = 'true';
+			}
 			
+			// Parse uncached tags in all fields and content
+			if ($processJsonFields) {
+				foreach ($fields as $key => $value) {
+					$modx->parser->processElementTags('', $fields[$key], true, true, '[[', ']]', array(), $maxIterations);
+				}
+			}
+			
+			$fields['content'] = $modx->resource->_output;
+					
+			$endtime= microtime();
+			$endtime= explode(" ", $endtime);
+			$endtime= $endtime[1] + $endtime[0];
+			$tsum= round(($endtime - $modx->startTime) * 1000, 0) . " ms";
+			
+			$fields['content-render-time'] = $tsum;
+
+			$modx->resource->_output = $modx->toJSON($fields);
+		
+		
+		
+		} else {
+		// non-ajax request
+				
+			// get resource output
+			$output = &$modx->resource->_output;
+			
+			// get template wrapper name from TV
+			$templateWrapper = $modx->resource->getTVValue('template.wrapper');
+			if ($templateWrapper == "") return;
+			
+			$output = $modx->parseChunk($templateWrapper,array('resource.content' => $output));
+
+			// parse all non-cacheable and remove unprocessed tags
+			$modx->getParser()->processElementTags('', $output, true, true, '[[', ']]', array(), $maxIterations);
+		
 		}
 	break;
 }
